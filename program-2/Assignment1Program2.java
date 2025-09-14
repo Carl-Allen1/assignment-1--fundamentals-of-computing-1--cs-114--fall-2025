c4367e6a4d27a4e079bec21ccca4b7c77fdfe6fa
CarlTheOdd <carl31415@outlook.com>
2025-09-14 11:09:31 -0400

    Took user input for base

--- program-2/Assignment1Program2.java
+++ program-2/Assignment1Program2.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Assignment1Program2 {
   public static void main(String[] args) {
+    System.out.println("Base Conversion Program");
 
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Please enter a base (2-9)");
+    int base = input.nextInt();
   }
 }
