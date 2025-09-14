045419e88856058fe4f39ff6fddf2e60d89ce687
CarlTheOdd <carl31415@outlook.com>
2025-09-14 10:03:26 -0400

    Step one completed

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,5 +1,13 @@
 public class Assignment1Program1 {
   public static void main(String[] args) {
-
+    System.out.println("  CCCCC         A    ");
+    System.out.println(" CCCCCCC       AAA   ");
+    System.out.println("CCC    CC     AA AA  ");
+    System.out.println("CC           AA   AA ");
+    System.out.println("CC          AAAAAAAAA");
+    System.out.println("CC          AA     AA");
+    System.out.println("CCC    CC   AA     AA");
+    System.out.println(" CCCCCCC    AA     AA");
+    System.out.println("  CCCCC     AA     AA");
   }
 }
