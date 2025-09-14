873b53a130e23c2e40e4adfbf71f279ef3e01b17
CarlTheOdd <carl31415@outlook.com>
2025-09-14 10:47:06 -0400

    Step two completed

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Assignment1Program1 {
   public static void main(String[] args) {
     System.out.println("  CCCCC         A    ");
@@ -9,5 +11,20 @@ public class Assignment1Program1 {
     System.out.println("CCC    CC   AA     AA");
     System.out.println(" CCCCCCC    AA     AA");
     System.out.println("  CCCCC     AA     AA");
+
+    Scanner input = new Scanner(System.in);
+    
+    System.out.println("Please enter a 5-character string: ");
+    String userString = input.nextLine();
+
+    String clippedString;
+    clippedString = userString.substring(1, 4);
+    
+    StringBuilder stringBuilder = new StringBuilder(clippedString);
+    String finalString = stringBuilder.reverse().toString();
+
+    System.out.println(finalString);
+
+    input.close();
   }
 }
